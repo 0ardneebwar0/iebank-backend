1f5a390a454f47e9c1fc3626fc104e9b6e8b10b7
0ardneebwar0 <116731908+0ardneebwar0@users.noreply.github.com>
2022-11-09 21:46:42 +0100

    Update test_routes.py

--- tests/functional/test_routes.py
+++ tests/functional/test_routes.py
@@ -9,6 +9,15 @@ def test_get_accounts(testing_client):
     """
     response = testing_client.get('/accounts')
     assert response.status_code == 200
+    
+def test_get_account_ONLY_ONE(testing_client):
+    """
+    GIVEN a Flask application
+    WHEN the '/accounts/<int:id>' page is requested (GET)
+    THEN check the response is valid
+    """
+    response = testing_client.get('/accounts/1')
+    assert response.status_code == 200
 
 def test_dummy_wrong_path():
     """
@@ -28,5 +37,33 @@ def test_create_account(testing_client):
     """
     response = testing_client.post('/accounts', json={'name': 'John Doe', 'currency': '€'})
     assert response.status_code == 200
+    
+def test_update_account(testing_client, id):
+    """
+    GIVEN a Flask application
+    WHEN the '/accounts' page is posted to (POST)
+    THEN check the response is valid
+    """
+    response = testing_client.put('/accounts/1', json={'name': 'Robin Hood', 'currency': '€'})
+    assert response.status_code == 200
+
+
+def test_delete_account(testing_client):
+    """
+    GIVEN a Flask application
+    WHEN the '/accounts' page is requested (GET)
+    THEN check the response is valid
+    """
+    response = testing_client.delete('/accounts/1')
+    assert response.status_code == 200
+
+def test_path_(testing_client):
+    """
+    GIVEN a Flask application
+    WHEN the '/accounts' page is requested (GET)
+    THEN check the response is valid
+    """
+    response = testing_client.post('/', json={'name': 'Brad Brown', 'currency': '$'})
+    assert response.status_code == 405
 
 
